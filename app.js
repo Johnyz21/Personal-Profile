cdd11e0cf4eab1043a91e73b181620bb85a4cc3c
marklajs <jjm21@hotmail.co.uk>
2017-12-20 09:33:15 -0500

    Added my logo to the menu. Changed the skills list such that they are a list of buttons, not links.

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use('/js',  express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/js',  express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/js', express.static(__dirname + '/node_modules/popper.js/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/particles.js'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
