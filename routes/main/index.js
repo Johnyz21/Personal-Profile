38cd77f8ab33c2ac8e4db78047fa5830dc56d21b
marklajs <jjm21@hotmail.co.uk>
2018-01-01 18:07:21 +0000

    Altered the text / img measurements to abide by responsive typography ( view unites )

--- routes/main/index.js
+++ routes/main/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('main/index', { title: 'Website!' });
+  res.render('main/index', { title: 'Jonathan Markland!' });
 });
 
 module.exports = router;
