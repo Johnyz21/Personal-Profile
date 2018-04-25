d0dec109d5b845b08bba4a15c65be05932bd152a
marklajs <jjm21@hotmail.co.uk>
2018-04-25 19:24:36 +0100

    Added google verification page

---
+++ routes/google_verification/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('google/google871789df72a533d2.html');
+});
+
+module.exports = router;
